32cd12c949f65dc8b0298efdd37ba8efafa0dcb3
zchengyun <1690132420@qq.com>
2021-09-05 22:24:19 +0800

    添加项目文件。

---
+++ type_traits.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <type_traits>//用于判断类型
+
+struct A
+{
+
+};
+
+int f();
+
+int main()
+{
+	std::cout << std::boolalpha;//输出为bool型
+	std::cout << std::is_void<void>::value << '\n';
+	std::cout << std::is_void<int>::value << '\n';
+
+	std::cout << std::is_class<A>::value << '\n';
+
+	std::cout << std::is_array<int[3]>::value << '\n';
+
+	std::cout << std::is_function<decltype(f)>::value << '\n';//选择并返回操作数的数据类型
+	std::cout << std::is_function<int>::value << '\n';
+}
+
